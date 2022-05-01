deb47205a151d4b6f539e1fb7f7d5960a841a83b
roniach019 <roniach019@gmail.com>
2022-05-01 11:02:34 +0600

    login text filed is added .. but text is not showing clearly in any device

--- lib/login.dart
+++ lib/login.dart
@@ -13,7 +13,65 @@ class _MyLoginState extends State<MyLogin> {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/login.png')
+          image:
+          AssetImage('assets/login.png'),
+            fit: BoxFit.cover
+        )
+      ),
+      child: Scaffold(
+        // transparent dile sada rong ke bodle immage bosabe
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(),
+        body: Stack(  // Column er bodole Stack
+          children: [
+            Container(
+              //padding: EdgeInsets.only(left: 35, top: 130),
+              child: Text(
+                'Wellcome\nBack',
+                style: TextStyle(color: Colors.white, fontSize: 33),
+              )
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only
+                    ( // size ta sob mobile soman vabe dekhanur jonno eiti
+                    top: MediaQuery.of(
+                    context).size.height*1,
+                    right: 30,
+                    left:35
+                    ),
+                // 2 ta text field deuar jonno column dibo
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey.shade100,
+                        filled: true,
+                        hintText: 'Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)
+                          )
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         )
       ),
     );
